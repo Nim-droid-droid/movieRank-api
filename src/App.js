7c1f098fe6fdba5a0e7fa25335e465ff6a111a57
Nim-droid-droid <levkoz2222@outlook.com>
2023-03-08 19:54:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import SideBar from "./components/Sidebar"
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Movies API!
+      </h1>
+      <SideBar />
+    </>
+  )
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+// create li from API 
+export default function Sidebarr(props){
+
+  return(
+    <>
+    <h1>Hello world</h1>
+    </>
+  )
+}
